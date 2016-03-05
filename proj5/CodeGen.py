462085f25d96f5fb6386fe46d43f80abf604968c
liuy42 <liuy42@miamioh.edu>
2016-03-05 12:50:10 -0500

    Skel code for code generator

---
+++ proj5/CodeGen.py
@@ -0,0 +1,46 @@
+from MLparser import *
+
+
+
+def codeGen(parse_tree, symbol_table, output):
+    fp = open("test.txt", "w")
+    start(symbol_table, fp)
+    traverse(parse_tree, symbol_table, fp)
+    finish(fp)
+    fp.close()
+
+
+def traverse(parse_tree, symbol_table, fp):
+    if parse_tree.children:
+        for child in parse_tree.children:
+            if child.label in func_factory:
+                func_factory[parse_tree.label](parse_tree, symbol_table, fp)
+            else:
+                for child in parse_tree.children:
+                    traverse(child, symbol_table, fp)
+
+
+def start(symbol_table, fp):
+    pass
+
+def finish(fp):
+    pass
+
+def read_id(parse_tree, symbol_table, fp):
+    pass
+
+def write_id(parse_tree, symbol_table, fp):
+    pass
+
+def assign(parse_tree, symbol_table, fp):
+    pass
+
+def expr_func(parse_tree, symbol_table, fp):
+    pass
+
+def process(parse_tree, symbol_table, fp):
+    pass
+
+func_factory = {"READ":read_id, "WRITE":write_id, "ASSIGN":assign, "EXPRESSION":expr_func, "ID": process}
+
+
