eb93a835e19d233c21c87db7c51d768bd84a0e3f
liuy42 <liuy42@miamioh.edu>
2016-04-28 20:05:54 -0400

    Fixed test syntax and semantic errors.

--- proj8/proj_8_testers/c_files/test8.c
+++ proj8/proj_8_testers/c_files/test8.c
@@ -1,6 +1,6 @@
 // Complex pass by reference
 
-int foo(int* x, int* y) {
+int foo(int& x, int& y) {
   x = x + 1;
   y = y + 1;
   return x + y;
@@ -8,7 +8,7 @@ int foo(int* x, int* y) {
 
 int main() {
   int a = 0;
-  int b = foo(a);
+  int b = foo(a, 4);
   printf("%d\t%d\n", a, b);
 
   return 0;
