9933d994803b492f7c94b2f06e011ae7e5c3d3dc
Caroline Danzi <danzicr@miamioh.edu>
2016-05-08 16:36:06 -0400

    Added semantic errors for function parameters

--- proj8/errors.py
+++ proj8/errors.py
@@ -60,6 +60,21 @@ class SemanticError(Exception):
         raise SemanticError('Semantic error: Incompatible Type for Function: Line num {:d}, column num: {:d}\n\t{:s} is'
                             ' of incompatible type {:s} for {:s}.'.format(line, col, var_name, type_name, func_name))
 
+    @staticmethod
+    def raise_parameter_type_mismatch(param_name, func_name, line, col):
+        raise SemanticError('Semantic error: Parameter Type Mismatch: Line num {:d}, column num: {:d}\n\t{:s} is'
+                            ' of incompatible type {:s} in function {:s}.'.format(line, col, param_name, type_name, func_name))
+
+    @staticmethod
+    def raise_parameter_number_mismatch(num_params_expected, func_name, line, col):
+        raise SemanticError('Semantic error: Parameter Number Mismatch: Line num {:d}, column num: {:d}\n\t{:s} should have'
+                            ' {:d} parameters.'.format(line, col, func_name, num_params_expected))
+
+    @staticmethod
+    def raise_expression_pass_by_ref(expression, func_name, line, col):
+        raise SemanticError('Semantic error: Illegal Pass By Reference: Line num {:d}, column num: {:d}\n\tIn {:s},'
+                            ' {:s} cannot be passed by reference.'.format(line, col, func_name, expression))
+
     def __init__(self, msg):
         self.msg = msg
 
